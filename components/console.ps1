d5528dc1eb60b56f2ea75efb2fa3190e4e39d808
Pete Deffendol <pete@deffendol.org>
2019-03-08 14:59:15 -0700

    Fix Set-PSReadlineOption parameters for latest Win10

--- components/console.ps1
+++ components/console.ps1
@@ -41,8 +41,8 @@ namespace dotfiles {
 
 function Verify-Elevated {
     # Get the ID and security principal of the current user account
-    $myIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $myPrincipal=new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+    $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myPrincipal = new-object System.Security.Principal.WindowsPrincipal($myIdentity)
     # Check to see if we are currently running "as Administrator"
     return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
@@ -50,7 +50,7 @@ function Verify-Elevated {
 function Verify-PowershellShortcut {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Path
     )
 
@@ -61,7 +61,7 @@ function Verify-PowershellShortcut {
     $shortcut = $shell.CreateShortcut("$(Resolve-Path $Path)")
 
     $result = ($shortcut.TargetPath -eq "$env:WINDIR\system32\WindowsPowerShell\v1.0\powershell.exe") -or `
-      ($shortcut.TargetPath -eq "$env:WINDIR\syswow64\WindowsPowerShell\v1.0\powershell.exe")
+    ($shortcut.TargetPath -eq "$env:WINDIR\syswow64\WindowsPowerShell\v1.0\powershell.exe")
     [Runtime.Interopservices.Marshal]::ReleaseComObject($shortcut) | Out-Null
     return $result
 }
@@ -69,7 +69,7 @@ function Verify-PowershellShortcut {
 function Verify-BashShortcut {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Path
     )
 
@@ -87,7 +87,7 @@ function Verify-BashShortcut {
 function Reset-PowerShellShortcut {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Path
     )
 
@@ -121,7 +121,7 @@ function Reset-PowerShellShortcut {
 function Reset-BashShortcut {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Path
     )
 
@@ -153,26 +153,26 @@ function Reset-BashShortcut {
 
 function Reset-AllPowerShellShortcuts {
     @(`
-        "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"`
+            "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"`
     ) | ForEach { Reset-PowerShellShortcut $_ }
 }
 
 function Reset-AllBashShortcuts {
     @(`
-        "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar",`
-        "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"`
+            "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar", `
+            "$ENV:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"`
     ) | ForEach { Reset-BashShortcut $_ }
 }
 
 function Convert-ConsoleColor {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string]$rgb
     )
 
@@ -180,24 +180,26 @@ function Convert-ConsoleColor {
         write-Error "Invalid color '$rgb' should be in RGB hex format, e.g. #000000"
         Return
     }
-    $num = [Convert]::ToInt32($rgb.substring(1,6), 16)
+    $num = [Convert]::ToInt32($rgb.substring(1, 6), 16)
     $bytes = [BitConverter]::GetBytes($num)
     [Array]::Reverse($bytes, 0, 3)
     return [BitConverter]::ToInt32($bytes, 0)
 }
 
 $PSReadLineRegistry = Get-ItemProperty 'HKCU:\Console\PSReadLine' -ErrorAction SilentlyContinue
-Set-PSReadlineOption -TokenKind Comment   -ForegroundColor ($PSReadLineRegistry.CommentForeground  , "DarkGreen" -ne $null)[0]
-Set-PSReadlineOption -TokenKind Keyword   -ForegroundColor ($PSReadLineRegistry.KeywordForeground  , "Green"     -ne $null)[0]
-Set-PSReadlineOption -TokenKind String    -ForegroundColor ($PSReadLineRegistry.StringForeground   , "DarkCyan"  -ne $null)[0]
-Set-PSReadlineOption -TokenKind Operator  -ForegroundColor ($PSReadLineRegistry.OperatorForeground , "DarkGray"  -ne $null)[0]
-Set-PSReadlineOption -TokenKind Variable  -ForegroundColor ($PSReadLineRegistry.VariableForeground , "Green"     -ne $null)[0]
-Set-PSReadlineOption -TokenKind Command   -ForegroundColor ($PSReadLineRegistry.CommandForeground  , "Yellow"    -ne $null)[0]
-Set-PSReadlineOption -TokenKind Parameter -ForegroundColor ($PSReadLineRegistry.ParameterForeground, "DarkGray"  -ne $null)[0]
-Set-PSReadlineOption -TokenKind Type      -ForegroundColor ($PSReadLineRegistry.TypeForeground     , "Gray"      -ne $null)[0]
-Set-PSReadlineOption -TokenKind Number    -ForegroundColor ($PSReadLineRegistry.NumberForeground   , "White"     -ne $null)[0]
-Set-PSReadlineOption -TokenKind Member    -ForegroundColor ($PSReadLineRegistry.MemberForeground   , "Gray"      -ne $null)[0]
-Set-PSReadlineOption -TokenKind None      -ForegroundColor ($PSReadLineRegistry.NormalForeground   , "$([System.Console]::ForegroundColor)" -ne $null)[0]
-Set-PSReadlineOption -EmphasisForegroundColor              ($PSReadLineRegistry.EmphasisForeground , "Cyan" -ne $null)[0]
-Set-PSReadlineOption -ErrorForegroundColor                 ($PSReadLineRegistry.ErrorForeground    , "Red"  -ne $null)[0]
+Set-PSReadlineOption -Colors @{
+    "Comment"      = ($PSReadLineRegistry.CommentForeground  , "DarkGreen" -ne $null)[0]
+    "Keyword"      = ($PSReadLineRegistry.KeywordForeground  , "Green" -ne $null)[0]
+    "String"       = ($PSReadLineRegistry.StringForeground   , "DarkCyan" -ne $null)[0]
+    "Operator"     = ($PSReadLineRegistry.OperatorForeground , "DarkGray" -ne $null)[0]
+    "Variable"     = ($PSReadLineRegistry.VariableForeground , "Green" -ne $null)[0]
+    "Command"      = ($PSReadLineRegistry.CommandForeground  , "Yellow" -ne $null)[0]
+    "Parameter"    = ($PSReadLineRegistry.ParameterForeground, "DarkGray" -ne $null)[0]
+    "Type"         = ($PSReadLineRegistry.TypeForeground     , "Gray" -ne $null)[0]
+    "Number"       = ($PSReadLineRegistry.NumberForeground   , "White" -ne $null)[0]
+    "Member"       = ($PSReadLineRegistry.MemberForeground   , "Gray" -ne $null)[0]
+    "DefaultToken" = ($PSReadLineRegistry.NormalForeground   , "$([System.Console]::ForegroundColor)" -ne $null)[0]
+    "Emphasis"     = ($PSReadLineRegistry.EmphasisForeground , "Cyan" -ne $null)[0]
+    "Error"        = ($PSReadLineRegistry.ErrorForeground    , "Red" -ne $null)[0]
+}
 Remove-Variable PSReadLineRegistry
